35a80368e1e6c2a931e4381eae7c73d4bda8db61
voidmahipal <mahipal7131@gmail.com>
2026-09-18 23:13:28 +0530

    Added the home page of the website

---
+++ src/components/Home.jsx
@@ -0,0 +1,19 @@
+function Home() {
+    return <>
+        <header>
+            <p>SHOP.CO</p>
+            <nav>
+                <a href="/">Home</a>
+                <a href="/">Shop</a>
+                <a href="/">Card</a>
+            </nav>
+        </header>
+        <main>
+            <p>NEW COLLECTION</p>
+            <h1>Minimal Pieces For A Better You</h1>
+            <button><a href="">Shop Now →</a></button>
+        </main>
+    </>
+}
+
+export {Home};
